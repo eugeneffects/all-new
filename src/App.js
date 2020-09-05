1cca36f76e0ce2130da0e9b44695e218fd8497a0
eugneffects <eugneffects@gmail.com>
2020-09-05 15:50:21 +0900

    setting:webpack,scss

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./assets/style.scss";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="title">절치부심</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname + "/dist"),
+  },
+  mode: "none",
+  module: {
+    rules: [
+      {
+        test: /\.(html)$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true },
+          },
+        ],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.(css)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "style-loader",
+          "css-loader",
+          "postcss-loader",
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: "./dist",
+    port: 3000,
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./public/index.html", // public/index.html 파일을 읽는다.
+      filename: "index.html", // output으로 출력할 파일은 index.html 이다.
+    }),
+    new MiniCssExtractPlugin({
+      filename: "./src/assets/style.scss",
+    }),
+  ],
+};
